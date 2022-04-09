1f8d7b1fa0c03cb7e053d4fc1755b402132696ba
Noah Mamié <noah.mamie@uzh.ch>
2022-04-09 16:12:40 +0200

    Tabular overview front-end, closes #9, closes #6

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,25 +1,31 @@
-import {useEffect, useState} from 'react';
-import {api, handleError} from 'helpers/api';
-import {Spinner} from 'components/ui/Spinner';
-import {Button} from 'components/ui/Button';
-import {useHistory} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { api, handleError } from "helpers/api";
+import { Spinner } from "components/ui/Spinner";
+import { Button } from "components/ui/Button";
+import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Game.scss";
-import {Link} from 'react-router-dom';
-import User from 'models/User';
+import { Link } from "react-router-dom";
+import User from "models/User";
+import Carpark from "models/Carpark";
 
-
-const Player = ({user}) => (
+const Player = ({ carpark }) => (
   <div className="player container">
-    <Link className="player username" to={'/profile/' + user.id}>{user.username}</Link>
-    <div className="player name">{user.name}</div>
-    <div className="player id">id: {user.id}</div>
+    <Link className="player name" to={"/carparks/" + carpark.carparkId}>
+      {carpark.name}
+    </Link>
+    <div className="player tariff">
+      {carpark.hourlyTariff} CHF
+    </div>
+    <div className="player capacity">
+      {carpark.numOfEmptySpaces}/{carpark.maxCapacity}
+    </div>
   </div>
 );
 
 Player.propTypes = {
-  user: PropTypes.object
+  carpark: PropTypes.object,
 };
 
 const Game = () => {
@@ -31,31 +37,34 @@ const Game = () => {
   // keep its value throughout render cycles.
   // a component can have as many state variables as you like.
   // more information can be found under https://reactjs.org/docs/hooks-state.html
-  const [users, setUsers] = useState(null);
+  const [carparks, setCarparks] = useState(null);
 
   const logout = () => {
-    let userId = localStorage.getItem('currentUser');
+    let userId = localStorage.getItem("currentUser");
 
     const doLogout = async () => {
       try {
         const response = await api.post(`/users/${userId}/logout`);
-  
+
         // Get the returned user
         const user = new User(response.data);
 
-        console.log("Status of user with id " + user.id + " is now: " + user.logged_in);
-  
-        // Logout successfully worked --> navigate to the route /login
-        localStorage.removeItem('token');
-        history.push('/login');
+        console.log(
+          "Status of user with id " + user.id + " is now: " + user.logged_in
+        );
 
+        // Logout successfully worked --> navigate to the route /login
+        localStorage.removeItem("token");
+        history.push("/login");
       } catch (error) {
-        alert(`Something went wrong during the logout: \n${handleError(error)}`);
+        alert(
+          `Something went wrong during the logout: \n${handleError(error)}`
+        );
       }
     };
 
     doLogout();
-  }
+  };
 
   // the effect hook can be used to react to change in your component.
   // in this case, the effect hook is only run once, the first time the component is mounted
@@ -65,46 +74,59 @@ const Game = () => {
     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
     async function fetchData() {
       try {
-        const response = await api.get('/users');
+        // fetch all carparks through REST API
+        const response = await api.get("/carparks");
 
         // delays continuous execution of an async operation for 1 second.
         // This is just a fake async call, so that the spinner can be displayed
         // feel free to remove it :)
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         // Get the returned users and update the state.
-        setUsers(response.data);
+        setCarparks(response.data);
 
         // This is just some data for you to see what is available.
         // Feel free to remove it.
-        console.log('request to:', response.request.responseURL);
-        console.log('status code:', response.status);
-        console.log('status text:', response.statusText);
-        console.log('requested data:', response.data);
+        console.log("request to:", response.request.responseURL);
+        console.log("status code:", response.status);
+        console.log("status text:", response.statusText);
+        console.log("requested data:", response.data);
 
         // See here to get more data.
         console.log(response);
       } catch (error) {
-        console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+        console.error(
+          `Something went wrong while fetching the users: \n${handleError(
+            error
+          )}`
+        );
         console.error("Details:", error);
-        alert("Something went wrong while fetching the users! See the console for details.");
+        alert(
+          "Something went wrong while fetching the users! See the console for details."
+        );
       }
     }
 
     fetchData();
   }, []);
 
-  let content = <Spinner/>;
+  let content = <Spinner />;
 
-  if (users) {
+  if (carparks) {
     content = (
       <div className="game">
+        <tr>
+          <th align="left">Name of Carpark</th>
+          <th align="center">Tariff (per hour)</th>
+          <th align="right">Current Capacity</th>
+        </tr>
         <ul className="game user-list">
-          {users.map(user => (
-            <Player user={user} key={user.id}/>
+          {carparks.map((carpark) => (
+            <Player carpark={carpark} key={carpark.carparkId} />
           ))}
         </ul>
         <Button
+          className="game logout-button"
           width="100%"
           onClick={() => logout()}
         >
@@ -116,13 +138,10 @@ const Game = () => {
 
   return (
     <BaseContainer className="game container">
-      <h2>User Overview</h2>
-      <p className="game paragraph">
-        Get all users from secure endpoint:
-      </p>
+      <h2>Carpark Overview</h2>
       {content}
     </BaseContainer>
   );
-}
+};
 
 export default Game;
