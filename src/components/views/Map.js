15a7be398a8200334603361254244a01717c6760
Noah Mamié <noah.mamie@uzh.ch>
2022-04-16 23:58:58 +0200

    Added markers and popup, closes #15, closes #17

--- src/components/views/Map.js
+++ src/components/views/Map.js
@@ -4,6 +4,7 @@ import { Spinner } from "components/ui/Spinner";
 import { Button } from "components/ui/Button";
 import { useHistory } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Map.scss";
@@ -12,25 +13,12 @@ import User from "models/User";
 import Carpark from "models/Carpark";
 import NavbarComp from "components/ui/NavbarComp";
 
-const Player = ({ carpark }) => (
-  <div className="player container">
-    <div>
-      <Link className="player name" to={"/carparks/" + carpark.carparkId}>
-        {carpark.name}
-      </Link>
-    </div>
-    <div className="player tariff">
-      {carpark.hourlyTariff} CHF
-    </div>
-    <div className="player capacity">
-      {carpark.numOfEmptySpaces}/{carpark.maxCapacity}
-    </div>
-  </div>
-);
-
-Player.propTypes = {
-  carpark: PropTypes.object,
-};
+// create a custom icon for the on-map markers
+// based on an svg file
+export const carparkIcon = new Icon({
+  iconUrl: "/carpark.svg",
+  iconSize: [50, 50],
+});
 
 const Map = () => {
   // use react-router-dom's hook to access the history
@@ -42,6 +30,7 @@ const Map = () => {
   // a component can have as many state variables as you like.
   // more information can be found under https://reactjs.org/docs/hooks-state.html
   const [carparks, setCarparks] = useState(null);
+  const [activePark, setActivePark] = useState(null);
 
   const logout = () => {
     let userId = localStorage.getItem("currentUser");
@@ -84,7 +73,7 @@ const Map = () => {
         // delays continuous execution of an async operation for 1 second.
         // This is just a fake async call, so that the spinner can be displayed
         // feel free to remove it :)
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        // await new Promise((resolve) => setTimeout(resolve, 1000));
 
         // Get the returned users and update the state.
         setCarparks(response.data);
@@ -116,26 +105,50 @@ const Map = () => {
 
   let content = <Spinner />;
 
+  // if the fetch function actually finds carparks in the DB
+  // the carparks are shown as custom markers on the map
+  // in case the carparks are clicked on,
+  // a popup with further options appears
   if (carparks) {
     content = (
-      <div className="game main">
-        <div className="game description">
-          <div className="game parkname">Name of Carpark</div>
-          <div className="game tariff">Tariff (per hour)</div>
-          <div className= "game capacity">Current Capacity</div>
-        </div>
-        <ul className="game user-list">
-          {carparks.map((carpark) => (
-            <Player carpark={carpark} key={carpark.carparkId} />
-          ))}
-        </ul>
-        <Button
-          className="game logout-button"
-          width="100%"
-          onClick={() => logout()}
-        >
-          Logout
-        </Button>
+      <div>
+        {carparks.map((carpark) => (
+          <Marker
+            key={carpark.carparkId}
+            position={[carpark.longitude, carpark.latitude]}
+            onClick={() => {
+              setActivePark(carpark);
+            }}
+            icon={carparkIcon}
+          >
+            {carpark && (
+              <Popup
+                position={[carpark.longitude, carpark.latitude]}
+                onClose={() => {
+                  setActivePark(null);
+                }}
+              >
+                <div>
+                  <h2>{carpark.name}</h2>
+                  <div className="player container">
+                      Capacity:&nbsp;{carpark.numOfEmptySpaces}/
+                      {carpark.maxCapacity}
+                  </div>
+                  <Button
+                    className="game logout-button"
+                    width="100%"
+                    onClick={() => history.push("/carparks/" + carpark.carparkId)}
+                  >
+                    Check-in and Reservation
+                  </Button>
+                  <p>
+                    <b>Remarks:</b> {carpark.remarks}
+                  </p>
+                </div>
+              </Popup>
+            )}
+          </Marker>
+        ))}
       </div>
     );
   }
@@ -143,16 +156,12 @@ const Map = () => {
   return (
     <div>
       <NavbarComp />
-      <MapContainer center={[47.3769, 8.5417]} zoom={12}>
+      <MapContainer center={[47.3769, 8.5417]} zoom={13}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[47.3769, 8.5417]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {content}
       </MapContainer>
       {/* <BaseContainer className="game container">
       <h2 className="game title">Carpark Overview</h2>
