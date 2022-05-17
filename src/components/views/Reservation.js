f7c10218ca7fff70e59c1431348313bf8f9a48a6
ksennr <91547019+ksennr@users.noreply.github.com>
2022-05-17 14:30:43 +0200

    Revert "Revert "Merge branch 'master' of github.com:sopra-fs22-group-03/sopra-fs22-group-03-client""
    This reverts commit f4bbbf4a70603f3bbe5e949ed2569bd56c5d944d.

--- src/components/views/Reservation.js
+++ src/components/views/Reservation.js
@@ -47,15 +47,15 @@ const Booking = props => {
                     </div>
                     <div className = "booking date">
                         <FormField 
-                        innerLabel = "date at beginning"
                         value = {checkinDate}
+                        type = "date"
                         onChange={x => setCheckinDate(x)}>
                         </FormField>
                     </div>
                     <div className = "booking time">
                         <FormField 
-                        innerLabel = "time at beginning"
                         value = {checkinTime}
+                        type = "time"
                         onChange={x => setCheckinTime(x)}>
                         </FormField>
                     </div>
@@ -66,15 +66,15 @@ const Booking = props => {
                     </div>
                     <div className = "booking date">
                         <FormField 
-                        innerLabel = "date at end"
                         value = {checkoutDate}
+                        type = "date"
                         onChange={x => setCheckoutDate(x)}>
                         </FormField>
                     </div>
                     <div className = "booking time">
                         <FormField 
-                        innerLabel = "time at end"
                         value = {checkoutTime}
+                        type = "time"
                         onChange={x => setCheckoutTime(x)}>
 
                         </FormField>
@@ -104,10 +104,11 @@ const ReservationUnit = props => {
 
     const [parkingData, setParkingData] = useState({});
     const [isTrue, setIsTrue] = useState(false);
+    const userId = localStorage.getItem("currentUser");
 
     useEffect(() => {
         async function fetchData() {
-            const response = await api.get(`/carparks/${props.data.carparkId}`); 
+            const response = await api.get(`/carparks/${props.data.carparkId}/${userId}`); 
             
             setParkingData(response.data);
         }
