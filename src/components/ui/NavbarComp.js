f1e3ea48a89e3aa6daccf0fe7557308d28ed8857
Noah Mamié <noah.mamie@uzh.ch>
2022-05-24 16:39:16 +0200

    Minor bug fix

--- src/components/ui/NavbarComp.js
+++ src/components/ui/NavbarComp.js
@@ -78,7 +78,7 @@ const NavbarComp = () => {
           "You have a new message, do you want to answer it right away?"
         )
       ) {
-        history.push("/notifications");
+        history.push("/notifications/");
       }
     }
   }
